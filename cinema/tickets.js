4746ce217e2ca9e8daabe242dcaa760aeed08e6d
Kiera Walton <kwalton@qa.com>
2021-02-01 08:37:25 +0000

    practicing js logic

--- cinema/tickets.js
+++ cinema/tickets.js
@@ -8,25 +8,30 @@ const basket = document.querySelector(".basket");
 const multiplyButton = document.querySelector("#mult");
 const clearBasket = document.querySelector("#clearHistory");
 
+const filmTitle = document.querySelector("#film option:checked").value;
 
 // return the values entered by the user
 const getValues = () => {
     return [Number.parseFloat(number2.value), Number.parseFloat(number1.value)];
+}
+const getFilmTitle = () => {
+    return filmTitle;
 }
 
 const printValue = (newResult) => {
     result.value = newResult;
 }
 const multiply = () => {
+    const film = getFilmTitle();
     const [num1, num2] = getValues();
     const newResult = num1 * num2;
     printValue(`£ ${newResult}`);
-    basket2(num2, num1, newResult);
+    basket2(num2, num1, newResult, film);
 }
 //add values to basket - print out in add to basket section
-const basket2 = (num1, num2, result) => {
+const basket2 = (num1, num2, result, film) => {
     const newBasket = document.createElement("p");
-    const newText = document.createTextNode(`${num2} tickets with price ${num1}, total: £${result}`)
+    const newText = document.createTextNode(`${num2} tickets for ${film} with price £${num1}, total: £${result}`)
     newBasket.appendChild(newText);
     basket.appendChild(newBasket);
 }
